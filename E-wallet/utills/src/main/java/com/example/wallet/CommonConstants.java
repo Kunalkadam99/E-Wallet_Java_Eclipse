90551b5e8967c28d6ac3f5106f4f8e50dbe656d4
Kunalkadam99 <97374864+Kunalkadam99@users.noreply.github.com>
2023-04-20 14:23:29 +0530

    Add files via upload

---
+++ E-wallet/utills/src/main/java/com/example/wallet/CommonConstants.java
@@ -0,0 +1,20 @@
+package com.example.wallet;
+
+public class CommonConstants {
+
+	public static final String USER_CREATION_TOPIC = "user_created";
+	
+	public static final String TRANSACTION_CREATION_TOPIC = "user_created";
+	
+	public static final String TRANSACTION_COMPLETION_TOPIC = "user_created";
+	
+	public static final String WALLET_UPDATED_TOPIC = "user_created";
+	
+	public static final String USER_CREATION_TOPIC_USERID = "userId";
+	
+	public static final String USER_CREATION_TOPIC_PHONE_NUMBER = "phoneNumber";
+	
+	public static final String USER_CREATION_TOPIC_IDENTIFIER_KEY = "userIdentifier";
+	
+	public static final String USER_CREATION_TOPIC_IDENTIFIER_VALUE = "identifierValue";
+}
